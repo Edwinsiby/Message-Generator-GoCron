6e431c4a113f63048cbac334b6955439ba397b10
Edwin <edwinsibyrajakumary@gmail.com>
2023-11-18 10:39:50 +0530

    test

--- main.go
+++ main.go
@@ -2,20 +2,54 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/jasonlvhit/gocron"
 	"github.com/robfig/cron"
 )
 
 func main() {
-	c := cron.New()
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
 
-	c.AddFunc("*/5 * * * * *", generateMessage)
-	c.AddFunc("*/10 * * * * *", generateMessage2)
+	r.GET("/events", func(c *gin.Context) {
+		c.Header("Content-Type", "text/event-stream")
+		c.Header("Cache-Control", "no-cache")
+		c.Header("Connection", "keep-alive")
+		c.Header("Access-Control-Allow-Origin", "*")
 
-	c.Start()
+		flusher, ok := c.Writer.(http.Flusher)
+		if !ok {
+			http.Error(c.Writer, "Streaming unsupported", http.StatusInternalServerError)
+			return
+		}
 
-	select {}
+		messageChan := make(chan string)
+
+		gocron.Every(5).Seconds().Do(func() {
+			messageChan <- "Hello! This is the message."
+		})
+
+		gocron.Start()
+
+		for {
+			select {
+			case message := <-messageChan:
+				fmt.Fprintf(c.Writer, "data: %s\n\n", message)
+				flusher.Flush()
+			case <-c.Writer.CloseNotify():
+				fmt.Println("page closed")
+				return
+			}
+		}
+	})
+
+	r.Run(":8080")
 }
 
 func generateMessage() {
@@ -33,54 +67,13 @@ func generateMessage2() {
 	fmt.Printf("[%s] %s\n", timestamp, message)
 }
 
-// package main
+func generateMessage3() {
+	c := cron.New()
 
-// import (
-// 	"fmt"
-// 	"net/http"
+	c.AddFunc("*/5 * * * * *", generateMessage)
+	c.AddFunc("*/10 * * * * *", generateMessage2)
 
-// 	"github.com/gin-gonic/gin"
-// 	"github.com/jasonlvhit/gocron"
-// )
+	c.Start()
 
-// func main() {
-// 	r := gin.Default()
-// 	r.LoadHTMLGlob("templates/*.html")
-// 	r.GET("/", func(c *gin.Context) {
-// 		c.HTML(http.StatusOK, "index.html", nil)
-// 	})
-
-// 	r.GET("/events", func(c *gin.Context) {
-// 		c.Header("Content-Type", "text/event-stream")
-// 		c.Header("Cache-Control", "no-cache")
-// 		c.Header("Connection", "keep-alive")
-// 		c.Header("Access-Control-Allow-Origin", "*")
-
-// 		flusher, ok := c.Writer.(http.Flusher)
-// 		if !ok {
-// 			http.Error(c.Writer, "Streaming unsupported", http.StatusInternalServerError)
-// 			return
-// 		}
-
-// 		messageChan := make(chan string)
-
-// 		gocron.Every(5).Seconds().Do(func() {
-// 			messageChan <- "Hello! This is the message."
-// 		})
-
-// 		gocron.Start()
-
-// 		for {
-// 			select {
-// 			case message := <-messageChan:
-// 				fmt.Fprintf(c.Writer, "data: %s\n\n", message)
-// 				flusher.Flush()
-// 			case <-c.Writer.CloseNotify():
-// 				fmt.Println("page closed")
-// 				return
-// 			}
-// 		}
-// 	})
-
-// 	r.Run(":8080")
-// }
+	select {}
+}
